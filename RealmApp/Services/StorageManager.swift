709aaff59c24cfbab548b553e392505d88d7bace
MitrichMan <122974224+MitrichMan@users.noreply.github.com>
2023-04-06 21:34:58 +0800

    Initial Commit

---
+++ RealmApp/Services/StorageManager.swift
@@ -0,0 +1,91 @@
+//
+//  StorageManager.swift
+//  RealmApp
+//
+//  Created by Alexey Efimov on 08.10.2021.
+//  Copyright © 2021 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class StorageManager {
+    static let shared = StorageManager()
+    let realm = try! Realm()
+    
+    private init() {}
+    
+    // MARK: - Task List
+    func save(_ taskLists: [TaskList]) {
+        write {
+            realm.add(taskLists)
+        }
+    }
+    
+    func save(_ taskListTitle: String, completion: (TaskList) -> Void) {
+        write {
+            let taskList = TaskList(value: [taskListTitle])
+            realm.add(taskList)
+            completion(taskList)
+        }
+    }
+    
+    func delete(_ taskList: TaskList) {
+        write {
+            realm.delete(taskList.tasks)
+            realm.delete(taskList)
+        }
+    }
+    
+    func edit(_ taskList: TaskList, newValue: String) {
+        write {
+            taskList.title = newValue
+        }
+    }
+
+    func done(_ taskList: TaskList) {
+        write {
+            taskList.tasks.setValue(true, forKey: "isComplete")
+        }
+    }
+
+    // MARK: - Tasks
+    func saveTask(_ taskTitle: String, withTaskNote taskNote: String, to taskList: TaskList, completion: (Task) -> Void) {
+        write {
+            let task = Task(value: [taskTitle, taskNote])
+            taskList.tasks.append(task)
+            completion(task)
+        }
+    }
+    
+    func deleteTask(_ task: Task) {
+        write {
+            realm.delete(task)
+        }
+    }
+    
+    func editTask(_ task: Task, newTitle: String, newNote: String) {
+        write {
+            task.title = newTitle
+            task.note = newNote
+        }
+    }
+    
+    func doneTask(_ task: Task) {
+        write {
+            task.isComplete.toggle()
+        }
+    }
+    
+    // MARK: - Core Data Saving support
+    private func write(completion: () -> Void) {
+        do {
+            try realm.write {
+                completion()
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+    }
+}
